9c53f2a91019e836a1a3d4b5936ed815edc8144e
Sandeep Sekhon <sandeepsekhon@gmail.com>
2017-10-03 16:12:31 +1100

    Added registrations for asp.net identity

--- DependencyInjectionUnity/App_Start/UnityConfig.cs
+++ DependencyInjectionUnity/App_Start/UnityConfig.cs
@@ -3,6 +3,12 @@ using Microsoft.Practices.Unity;
 using Unity.Mvc5;
 using DependencyInjectionUnity.Contracts;
 using DependencyInjectionUnity.Providers;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using DependencyInjectionUnity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web;
 
 namespace DependencyInjectionUnity
 {
@@ -12,11 +18,11 @@ namespace DependencyInjectionUnity
         {
 			var container = new UnityContainer();
             
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
             container.RegisterType<IMyProvider, MyProvider>();
-            
+            var accountInjectionConstructor = new InjectionConstructor(new ApplicationDbContext());
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(accountInjectionConstructor);
+            container.RegisterType<IRoleStore<IdentityRole, string>, RoleStore<IdentityRole>>(accountInjectionConstructor);
+            container.RegisterType<IAuthenticationManager>(new InjectionFactory(o => System.Web.HttpContext.Current.GetOwinContext().Authentication));
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
